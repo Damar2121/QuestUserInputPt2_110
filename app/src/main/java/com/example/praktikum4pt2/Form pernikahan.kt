aa6e4ab1d5334b2d92f2da4aa8ead9d5ac8ea989
Damar2121 <damar.vivoy51@gmail.com>
2025-10-31 17:27:37 +0700

    menambah library runtime

--- app/src/main/java/com/example/praktikum4pt2/Form pernikahan.kt
+++ app/src/main/java/com/example/praktikum4pt2/Form pernikahan.kt
@@ -2,6 +2,7 @@ package com.example.praktikum4pt2
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
 
 @Composable
 fun FormPendaftaranScreen(modifier: Modifier = Modifier) {
