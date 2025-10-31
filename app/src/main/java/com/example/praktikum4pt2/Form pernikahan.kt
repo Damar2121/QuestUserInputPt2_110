9ead0c69cc39c1c03472845cfb892c4da5b1abaf
Damar2121 <damar.vivoy51@gmail.com>
2025-10-31 17:29:16 +0700

    nambahin definisi warna custom

--- app/src/main/java/com/example/praktikum4pt2/Form pernikahan.kt
+++ app/src/main/java/com/example/praktikum4pt2/Form pernikahan.kt
@@ -3,6 +3,11 @@ package com.example.praktikum4pt2
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+
+val PurpleHeader = Color(0xFF6A00C9)
+val LightPurpleBackground = Color(0xFFF0E6FF)
+val PurpleButton = Color(0xFF7B1FA2)
 
 @Composable
 fun FormPendaftaranScreen(modifier: Modifier = Modifier) {
